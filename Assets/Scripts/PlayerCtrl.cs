b2ecfa9a54c5339958ddb31338ba706e3fc48019
Oleksandr <teranase@gmail.com>
2019-03-28 19:30:01 +0200

    Start working on player control

---
+++ Assets/Scripts/PlayerCtrl.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCtrl : MonoBehaviour
+{
+    [Tooltip("this is an positive integer, speed boost")]
+    public int speedBoost;
+
+    private Rigidbody2D rigidbody;
+
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float playerSpeed = Input.GetAxisRaw("Horizontal");
+        if (playerSpeed != 0)
+            MoveHorizontal(playerSpeed);
+        else
+            StopMoving();
+    }
+
+    void MoveHorizontal(float playerSpeed)
+    {
+        rigidbody.velocity = new Vector2();
+    }
+
+    void StopMoving()
+    {
+        rigidbody.velocity = new Vector2(2, rigidbody.velocity.y);
+    }
+}
